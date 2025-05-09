7a24531f4628e9f968db8255839c4c0a0e005a97
nandinisingh27 <nandinisingh52891@gmail.com>
2025-05-09 20:01:51 +0530

    ready

---
+++ vital_care/secrets.py
@@ -0,0 +1,28 @@
+SECRET_KEY ='django-insecure-#deli9rx3^zv-g=5t#yw_)+bcw$a469kp$1og39jjoa_)h#v_4'
+# NAME="vital_care_db"
+# HOST="127.0.0.1"
+# PORT="3306"
+# USER="root"
+ENGINE="django.db.backends.mysql"
+# PASSWORD ="Nandini@2005"
+
+
+NAME="sql12777831"
+USER="sql12777831"
+PASSWORD="b4lkJYjs2s"
+HOST=" sql12.freesqldatabase.com"
+PORT=3306
+
+
+
+RAZOR_KEY_ID = 'rzp_test_txb4z4k8QaMOgb'
+RAZOR_KEY_SECRET ='ZQf99OkbmLpnYWA3AodTJhjN'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nandinisingh52891@gmail.com'
+EMAIL_HOST_PASSWORD = 'lgnd gwli oyle qfjm'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
